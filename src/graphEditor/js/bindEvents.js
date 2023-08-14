64b653b352ec8071d069cfefe43977c627a4f3dc
vezzzing <1932966162@qq.com>
2023-08-14 17:49:48 +0800

    添加色彩模式

--- src/graphEditor/js/bindEvents.js
+++ src/graphEditor/js/bindEvents.js
@@ -1,10 +1,21 @@
 import { Graph } from "./graph/graph";
 
+// 界面色彩配置
+let darkMode = true;
+
 /**
  * 
  * @param {Graph} graph initGraph()返回的图谱对象
  */
 export function bindEvents(graph) {
+    document.querySelector("#btnReverseMode").addEventListener("click", () => {
+        darkMode = !darkMode;
+        if (darkMode) {
+            document.querySelector(".mainWindow").classList="mainWindow darkMode";
+        }else{
+            document.querySelector(".mainWindow").classList="mainWindow lightMode";
+        }
+    });
     document.querySelector("#btnToJson").addEventListener("click", () => {
         console.log(graph.toJson());
     });
@@ -13,7 +24,7 @@ export function bindEvents(graph) {
     });
     document.querySelector("#bgColorInput").addEventListener("input", () => {
         graph.setBgColor(document.querySelector("#bgColorInput").value);
-    })
+    });
     window.oncontextmenu = function (e) {
         //取消默认的浏览器自带右键 很重要！！
         e.preventDefault();
