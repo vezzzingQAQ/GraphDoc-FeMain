5d87259ee35cdc249f1021124c1840222a0e1eb5
vezzzing <1932966162@qq.com>
2023-08-16 16:05:42 +0800

    包含外链的截图

--- src/graphEditor/js/graph/graph.js
+++ src/graphEditor/js/graph/graph.js
@@ -749,6 +749,11 @@ export class Graph {
             .style("width", "max-content")
             .style("height", "max-content")
             .style("flex-direction", "column")
+            .style("margin", 0)
+            .style("padding", 0)
+        domAddedSubComponentContainer.selectAll("*")
+            .style("margin", 0)
+            .style("padding", 0)
 
         addedSubComponentForeign
             .attr("width", function () {
@@ -911,9 +916,25 @@ export class Graph {
      * 下载为图片
      */
     exportImg(scale = 5) {
-        saveSvgAsPng(this.renderProperties.svg.node(), "vezz.png", {
-            scale: scale
-        });
+        // saveSvgAsPng(this.renderProperties.svg.node(), "vezz.png", {
+        //     scale: scale
+        // });
+        const svg = document.querySelector('svg');
+        const source = new XMLSerializer().serializeToString(svg); //将整个SVG document 对象序列化为一个 XML 字符串。
+        onDownload(source, 'text/xml', 'test.svg'); // 下载 
+        function onDownload(data, type, name) {
+            const blob = new Blob([data], { type }); // 返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+            const url = window.URL.createObjectURL(blob); //创建一个url
+            const link = document.createElement('a'); //创建一个a标签
+            link.href = url; // 把url 赋值给a标签的href
+            link.style.display = 'none';
+            link.setAttribute('download', name);
+            document.body.appendChild(link);
+
+            link.click(); // 触发a标签的点击事件
+            URL.revokeObjectURL(url); // 清除Url
+            document.body.removeChild(link);
+        };
     }
 
     /**
