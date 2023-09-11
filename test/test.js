fa3a57725a96c26e360f135d98614221a10c4542
vezzzing <1932966162@qq.com>
2023-09-11 10:37:52 +0800

    添加测试用例

--- test/test.js
+++ test/test.js
@@ -4,10 +4,17 @@ function main() {
     });
 
     let centerNode = new VNode();
+    centerNode.components["physics_node"]["fixPosition"] = false;
+    centerNode.components["exterior_node"]["sizeAuto"] = false;
+    centerNode.components["exterior_node"]["size"] = { x: 20, y: 10 };
+    centerNode.components["exterior_node"]["bgColor"] = `rgb(255,255,255)`;
+    centerNode.components["exterior_node"]["strokeColor"] = `rgb(0,0,0)`;
 
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 800; i++) {
         let node = new VNode();
         node.components["physics_node"]["fixPosition"] = false;
+        node.components["exterior_node"]["sizeAuto"] = false;
+        node.components["exterior_node"]["size"] = { x: Math.random() * 8 + 2, y: 10 };
         node.components["exterior_node"]["bgColor"] = `rgb(${Math.sin(i / 200) * 125 + 125},100,${255 - Math.sin(i / 200) * 125 + 125})`;
         node.components["exterior_node"]["strokeColor"] = `rgb(0,0,0)`;
         node.x = Math.sin(i) * 500;
