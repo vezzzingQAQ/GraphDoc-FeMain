7a011a27d9ae66176bd50a0c4097e2aa6e47e96c
vezzzing <1932966162@qq.com>
2023-10-15 18:29:25 +0800

    APP模式修复用户无法登录bug

--- src/public/js/tools.js
+++ src/public/js/tools.js
@@ -1,27 +1,40 @@
 // 获取指定名称的cookie
 export function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    if (process.env.RUN_ENV == "web") {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    } else {
+        return window.localStorage.getItem(cname);
     }
-    return "";
 }
 
 // 写入cookie
 export function setCookie(name, data, expireTime) {
-    let date = new Date();
-    date.setTime(date.getTime() + expireTime);
-    document.cookie = `${name}=${data}; expires=${date}`;
+    if (process.env.RUN_ENV == "web") {
+        let date = new Date();
+        date.setTime(date.getTime() + expireTime);
+        document.cookie = `${name}=${data}; expires=${date}`;
+    } else {
+        // app用localStorage进行储存
+        window.localStorage.setItem(name, data);
+    }
 }
 
 // 删除cookie
 export function delCookie(name) {
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cval = getCookie(name);
-    if (cval != null) document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+    if (process.env.RUN_ENV == "web") {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = getCookie(name);
+        if (cval != null) document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+    } else {
+        window.localStorage.removeItem(name);
+    }
 }
 
 // 获取get参数
