1f48673ed297c1ddc4aaad63b13cf0f5806b4acd
vezzzing <1932966162@qq.com>
2023-10-24 19:03:19 +0800

    模块拆分

---
+++ src/graphEditor/js/templateDyaList.js
@@ -0,0 +1,30 @@
+export const templateDyaList = [
+    {
+        showName: "碱基对",
+        name: "gen1",
+    },
+    {
+        showName: "年份图",
+        name: "dyaData",
+    },
+    {
+        showName: "CE图",
+        name: "centerGraphDya",
+    },
+    {
+        showName: "奥利奥",
+        name: "oreo",
+    },
+    {
+        showName: "格式化文本1",
+        name: "blockText",
+    },
+    {
+        showName: "格式化文本2",
+        name: "blockText2",
+    },
+    {
+        showName: "词云",
+        name: "wordCloud",
+    }
+]
---
+++ src/graphEditor/js/templateList.js
@@ -0,0 +1,30 @@
+export const templateList = [
+    {
+        showName: "节点示例",
+        name: "nodeEx",
+    },
+    {
+        showName: "关系图",
+        name: "centerGraph",
+    },
+    {
+        showName: "图谱",
+        name: "graph",
+    },
+    {
+        showName: "钢琴",
+        name: "piano",
+    },
+    {
+        showName: "考拉兹猜想",
+        name: "cc1",
+    },
+    {
+        showName: "圆周率",
+        name: "pie",
+    },
+    {
+        showName: "中国行政区划",
+        name: "cdc",
+    },
+];
